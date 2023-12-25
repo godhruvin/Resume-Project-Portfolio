4edec7b1c791050bc8bf8b2b78474e8c35d1e493
godhruvin <dhruvinmehta53@gmail.com>
2023-12-25 12:58:02 +0530

    project done

---
+++ src/components/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" id="About" className='w-full h-screen bg-gradient-to-b from-gray-800
+    to-black text-white'>
+      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+        </div>
+        <p className='text-xl mt-20'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, aut itaque illum error nostrum minima cumque nesciunt ex! At magni, tempore amet ea facere placeat ducimus animi reiciendis harum aspernatur exercitationem ipsum quaerat quis aperiam iure, voluptates delectus? Pariatur, architecto blanditiis. Minus adipisci sunt pariatur quam! Repellendus et rem eligendi?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, aut itaque illum error nostrum minima cumque nesciunt ex! At magni, tempore amet ea facere placeat ducimus animi reiciendis harum aspernatur exercitationem tiis. Minus adipisci sunt pariatur quam! Repellendus et rem eligendi?
+        </p>
+        <br />
+        <p className='text-xl'>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae voluptatem ipsam obcaecati voluptate placeat distinctio fugiat sequi facere nisi est amet ut laboriosam, similique optio non quasi itaque accusantium mollitia at. Delectus ratione perspiciatis necessitatibus veniam commodi iusto doloremque adipisci autem voluptatem sunt, ducimus aut dolorum repudiandae aliquam quasi culpa.
+        </p>
+      </div>
+
+    </div>
+  )
+}
+
+export default About
